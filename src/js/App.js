43118ff3b74713974a128c5d99d29ee5f03cd376
Martins Akeredolu <olamideakeredolu@gmail.com>
2020-10-16 09:37:06 +0100

    step 7

--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 const App = () => {
     return (
-        <h1>Hello World</h1>
+        <Carousel >
+            <div>
+                <h1>Hello World</h1>
+            </div>
+            <div>
+                <h1>It's me</h1>
+            </div>
+            <div>
+                <h1>Martins</h1>
+            </div>
+        </Carousel>
+
     );
 }
 
